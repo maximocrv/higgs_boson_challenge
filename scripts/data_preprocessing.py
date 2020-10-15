0b905f19387a03ccec2549d55b862dbfb6dae78d
Maximo Cravero <maximo@geronimo.ai>
2020-10-15 17:43:12 +0200

    add some stuff to pipeline to do list and script for data preprocessing methods

---
+++ scripts/data_preprocessing.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from scripts.proj1_helpers import *
+
+y, x, ids = load_csv_data("data/train.csv")
+
+
+def nan_to_mean(x):
+    raise NotImplementedError
+
+
+def nan_to_0(x):
+    raise NotImplementedError
+
+
+def remove_nan_features(x):
+    raise NotImplementedError
+
