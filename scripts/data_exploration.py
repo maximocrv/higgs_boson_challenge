1238f7641ebfe5492e0677f372c1dfae74580850
miloo42 <milo.imbeni@epfl.ch>
2020-10-17 14:28:35 +0200

    data exp

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -4,12 +4,34 @@ from scripts.proj1_helpers import *
 
 y, x, ids = load_csv_data("data/train.csv")
 
+
 def count_nan(x):
     """
 
-    :param x:
-    :return:
+    :param x: input
+    :return: ratios of nan
     """
-    return np.sum(np.isnan(x),axis=0) / x.shape(0)
+    x[x == -999] = np.nan
+    truth_array = np.isnan(x)
+    return (np.sum(truth_array, axis=0) )/ x.shape[0]
 
+def check_nan_positions(x,candidates):
+    "check if nan occurs in the same place, outputs the percentage of values with nans in all the candidates columns"
 
+    c= np.isnan(x[:,candidates])
+
+    check = np.sum(c,1) == candidates.shape[0]
+
+    value = np.sum(check)/ check.shape[0]
+
+    return value
+
+x[x == -999] = np.nan
+candidates= np.array([0, 23,24,25, 4,5,6,12,26,27,28])
+value1 = check_nan_positions(x,candidates)
+
+candidates= np.array([0, 23,24,25])
+value2 = check_nan_positions(x,candidates)
+
+candidates= np.array([0, 4,5,6,12,26,27,28])
+value3 = check_nan_positions(x,candidates)
