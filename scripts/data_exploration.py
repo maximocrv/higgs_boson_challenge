5b52fb98fb4ec4bca881e82f8f40e2c5a90b4532
Maximo Cravero <maximo@geronimo.ai>
2020-10-17 14:23:32 +0200

    add some helper functions and script for least_squares_gd

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -4,12 +4,15 @@ from scripts.proj1_helpers import *
 
 y, x, ids = load_csv_data("data/train.csv")
 
+
 def count_nan(x):
     """
 
     :param x:
     :return:
     """
-    return np.sum(np.isnan(x),axis=0) / x.shape(0)
+    return np.sum(x[x == -999], axis=0) / x.shape[0]
+
+
 
 
