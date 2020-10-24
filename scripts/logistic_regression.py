f1c27d7d1622b652c2cbe9a076f25b397fa7794c
Maximo Cravero <maximo@geronimo.ai>
2020-10-24 15:56:32 +0200

    Adding run scripts

--- scripts/logistic_regression.py
+++ scripts/logistic_regression.py
@@ -3,6 +3,7 @@ import numpy as np
 from scripts.proj1_helpers import load_csv_data
 from scripts.data_preprocessing import build_k_indices
 from scripts.implementations import logistic_regression_GD, logistic_regression_SGD, cross_validation
+from scripts.utilities import obtain_best_params
 
 y_tr, x_tr, ids_tr = load_csv_data("data/train.csv", mode='one_hot')
 
@@ -47,46 +48,4 @@ for h, gamma in enumerate(gammas):
         # accuracy_ranking[h,i]=np.mean(temp_acc)-2*np.std(temp_acc)
         accuracy_ranking[h, i] = np.mean(temp_acc)
 
-# max_ind = np.unravel_index(np.argmax(accuracy_ranking), accuracy_ranking.shape)
-
-# gamma_ind = max_ind[0]
-# gamma = gammas[gamma_ind]
-
-# degree_ind = max_ind[1]
-# degree = degrees[degree_ind]
-
-# if len(max_ind) > 2:
-    # lambda_ind = max_ind[2]
-    # lambda_ = lambdas[lambda_ind]
-#
-# degree_ind = max_ind[1]
-# degree = degrees[degree_ind]
-#
-# if len(max_ind) > 2:
-#     lambda_ind = max_ind[2]
-#     lambda_ = lambdas[lambda_ind]
-# # x_tr and y_tr already balanced from nans and standardized
-# tx_tr_tot = multi_build_poly(x_tr, degree)
-# # standardize again after polynomial basis expansion
-# tx_tr_tot = standardize_data(tx_tr_tot[:, 1:])
-# tx_tr_tot = np.concatenate((np.ones((tx_tr_tot.shape[0], 1)), tx_tr_tot), axis=1)
-#
-# w0 = np.random.randn(tx_tr_tot.shape[1])
-# nll, w = stochastic_gradient_descent(y_tr, tx_tr_tot, w0, max_iters=50000, gamma=gamma, batch_size=1,
-#                                      mode='logistic_reg')
-# w = w[-1]
-#
-#
-# y_te, x_te, ids_te = load_csv_data("data/test.csv", mode='one_hot')
-# x_te = np.delete(x_te, features, axis=1)
-# # nan to mean or median
-# x_te = standardize_data(x_te, nan_mode=nan_mode)
-#
-# tx_te_tot = multi_build_poly(x_te, degree)
-# tx_te_tot = standardize_data(tx_te_tot[:, 1:], nan_mode=nan_mode)
-# tx_te_tot = np.concatenate((np.ones((tx_te_tot.shape[0], 1)), tx_te_tot), axis=1)
-#
-# y_te_pred = predict_labels(w, tx_te_tot, mode='one_hot')
-# y_te_pred[y_te_pred == 0] = -1
-#
-# create_csv_submission(ids_te, y_te_pred, 'log_reg_sgd_submission.csv')
+best_params = obtain_best_params(accuracy_ranking, gammas, degrees, None)
--- scripts/run.py
+++ scripts/run.py
@@ -1,2 +1,64 @@
 # Script to run our best performing model with config etc etc
 import numpy as np
+
+from scripts.data_preprocessing import preprocess_data, split_data_jet, transform_data
+from scripts.implementations import least_squares
+from scripts.proj1_helpers import load_csv_data
+
+# RUN SETTINGS
+from scripts.utilities import compute_accuracy
+
+split_mode = 'default'
+binary_mode = 'one_hot'
+nan_mode = 'median'
+degree = 5
+
+method = least_squares
+
+x_tr, y_tr, inds_tr = load_csv_data('data/train.csv', mode=binary_mode)
+x_te, y_te, inds_te = load_csv_data('data/test.csv', mode=binary_mode)
+
+
+if split_mode == 'default':
+    x_tr = preprocess_data(x_tr, nan_mode=nan_mode)
+    x_te = preprocess_data(x_te, nan_mode=nan_mode)
+
+    x_tr, x_te = transform_data(x_tr, x_te, degree)
+
+    loss_tr, w = method(y_tr, x_tr, **kwargs)
+
+    # y_tr_pred = predict_labels(w, x_tr, binary_mode=binary_mode)
+    # y_te_pred = predict_labels(w, x_te, binary_mode=binary_mode)
+
+    # loss_te = compute_mse(y_te, x_te, w)
+
+    acc_tr = compute_accuracy(w, x_tr, y_tr, binary_mode=binary_mode)
+    acc_te = compute_accuracy(w, x_te, y_te, binary_mode=binary_mode)
+
+elif split_mode == 'jet_groups':
+    y_train_pred = np.zeros(len(y_tr))
+    y_test_pred = np.zeros(len(y_te))
+
+    jet_groups_tr = split_data_jet(x_tr)
+    jet_groups_te = split_data_jet(x_te)
+
+    for jet_group_tr, jet_group_te in zip(jet_groups_tr, jet_groups_te):
+        _x_tr = x_tr[jet_group_tr]
+        _x_te = x_te[jet_group_te]
+        _y_tr = y_tr[jet_group_tr]
+        _y_te = y_te[jet_group_te]
+
+        _x_tr = preprocess_data(_x_tr, nan_mode=nan_mode)
+        _x_te = preprocess_data(_x_te, nan_mode=nan_mode)
+
+        _x_tr, _x_te = transform_data(_x_tr, _x_te, degree)
+
+        loss_tr, w = method(_y_tr, _x_tr, **kwargs)
+
+        y_train_pred[jet_group_tr] = predict_labels(w, _x_tr, binary_mode=binary_mode)
+        y_test_pred[jet_group_te] = predict_labels(w, _x_te, binary_mode=binary_mode)
+
+        # loss_te = compute_mse(_y_te, _x_te, w)
+
+    acc_tr = len(np.where(y_train_pred - y_tr == 0)[0]) / y_train_pred.shape[0]
+    acc_te = len(np.where(y_test_pred - y_te == 0)[0]) / y_test_pred.shape[0]
